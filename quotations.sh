76e83e2d20e411871418a4d0e24decca5b85e5a9
Jashan Singh <jashan0809.singh@gmail.com>
2026-04-14 10:25:11 -0500

    i have lost my touch with commit messages after i got laughed on by my so called 'friends' i dont know what to type but i did work on userinput lets go

--- quotations.sh
+++ quotations.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
-#
-
 name="King"
 
 echo "Hello $name (double quotes)"
+
 echo 'Hello $name (single quotes)'
 
 #double quotes give the variable instead of the literal meaning
 
-#single quotes are the literal strings
+#single quotes are the literal strings, very simple so like YOU SHOULD UNDERSTAND(anybody looking at this for some reason)
---
+++ userinput.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "what are you trying to do"
+
+read -p ""
+
+
+
+# takes in user input, -p combines echo with read so it gives a message
