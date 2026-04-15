8d371b2c70b41a4f76a28cb57412d25a9b5be1c0
Jashan Singh <jashan0809.singh@gmail.com>
2026-04-15 10:24:45 -0500

    whelp printf is confusing but echo is weirder,- einstien probably

--- userinput.sh
+++ userinput.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "what are you trying to do" attempt_wsp
+if ((attempt_wsp !n))
 
-
-printf()
+echo "$attempt_wsp is cool "
 # takes in user input, -p combines echo with read so it gives a message
