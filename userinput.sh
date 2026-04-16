617e66907f32b14c8cdae197e2d7d08434d7188b
Jashan Singh <jashan0809.singh@gmail.com>
2026-04-16 10:29:16 -0500

    idk syntax still and its getiing annoying(fixed syntax0

--- userinput.sh
+++ userinput.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 read -p "what are you trying to do" attempt_wsp
-if ((attempt_wsp !n))
+if (( ! -n attempt_wsp))
 
-echo "$attempt_wsp is cool "
+echo "$attempt_wsp is cool"
 # takes in user input, -p combines echo with read so it gives a message
 
 
