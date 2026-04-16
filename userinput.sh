a7cf148f92687db509754ab5a9d186e4d3c7e9ce
Jashan Singh <jashan0809.singh@gmail.com>
2026-04-16 10:38:49 -0500

    test2?

--- userinput.sh
+++ userinput.sh
@@ -2,10 +2,14 @@
 
 read -p "what are you trying to do" attempt_wsp
 if (( ! -n attempt_wsp))
+	echo "type something loser"
 
 echo "$attempt_wsp is cool"
 # takes in user input, -p combines echo with read so it gives a message
 
 
-
+#NOTE TO MAKE THE SCRIPT LOOP OR IM COOKED
 #idk what the !n does, i searched it up im looooost
+
+#-n means if the string is not empty?
+#! reverses 
