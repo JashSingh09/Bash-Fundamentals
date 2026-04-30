06c5fca350f7c7c032e908eca9e3bc711ed9e0e7
Jashan Singh <jashan0809.singh@gmail.com>
2026-04-30 10:05:50 -0500

    note to self, make notes look nice by spamming dashes and other stuff

--- brackets.sh
+++ brackets.sh
@@ -5,8 +5,15 @@
 
 #square brackets[]
 
+#-------------------------------------------------------------------------------
+#double square brackets[[]]
+
+echo [whatsup ]
 #round()
-
+#single round brackets open a subshell, a shell inside a sheel but only outputs the answer and doesnt show the parth to get the answer
 #double round brackets(())
-
+#-------------------------------------------------------------------------------
 # curly {}
+#dollar curly ${}
+
+#
