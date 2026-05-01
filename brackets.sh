75f51515db2a2edf620f1775247cce162bff42cb
Jashan Singh <jashan0809.singh@gmail.com>
2026-05-01 10:08:06 -0500

    i wonder if git -am works, commit messages can be so boring tho(had to throw that in there becaues ima shallow person and I need to make it funny)

--- brackets.sh
+++ brackets.sh
@@ -8,12 +8,18 @@
 #-------------------------------------------------------------------------------
 #double square brackets[[]]
 
-echo [whatsup ]
+print ${what the helly}
+
 #round()
 #single round brackets open a subshell, a shell inside a sheel but only outputs the answer and doesnt show the parth to get the answer
 #double round brackets(())
 #-------------------------------------------------------------------------------
+
+
+
 # curly {}
+echo "(anything at this point works 
+
 #dollar curly ${}
 
 #
