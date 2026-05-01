7304b60d251a7040c703d23fdfd647c5139dd9b6
Jashan Singh <jashan0809.singh@gmail.com>
2026-05-01 10:11:48 -0500

    ball knowledge unlocked, new achievment, and going to start updating brackets but thats less important

--- brackets.sh
+++ brackets.sh
@@ -9,17 +9,18 @@
 #double square brackets[[]]
 
 print ${what the helly}
-
+read -p idk_brp
 #round()
-#single round brackets open a subshell, a shell inside a sheel but only outputs the answer and doesnt show the parth to get the answer
+#single round brackets open a subshell, a shell inside  sheel but only outputs the answer and doesnt show the parth to get the answer
 #double round brackets(())
 #-------------------------------------------------------------------------------
 
 
 
 # curly {}
-echo "(anything at this point works 
+echo "(anything at this point works "
 
 #dollar curly ${}
 
-#
+#I GOT THE ELITE BALL KNOWLEDGE NOW, THIS IS UNRELATED BUT CLICKING INSERT TURNS THE CURSOR TO OVERWRITE MODE
+#ITS KIND OF USELSS BUT STILL
