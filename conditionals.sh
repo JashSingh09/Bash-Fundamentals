4655da2229127ab73905dcd44d22324bda935474
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-05 10:12:08 -0500

    i had to redo things because i forgot to pull, what am i doing with my life

---
+++ conditionals.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+# conditionals are basically just logic tests, like boolean logic gates, like if a number is greater than 5 do something, thats it in simplest terms,
+
+#works with if commands
+
