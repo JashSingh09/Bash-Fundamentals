a26a3a2bbb7d516230e6f2b51000804b017320d5
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-05 10:36:38 -0500

    forgot to put what happens if you DONT input a number, which should have been the first thing i looked for, like LOGIC!!!

--- conditionals.sh
+++ conditionals.sh
@@ -8,19 +8,31 @@
 
 
 read -p "what temperature is the human body in celsius " users_guess
+if [[ "$users_guess" =~ ^[0-9]+$ ]]
 
-if [[ "$users_guess" -lt 37 ]]; then
+	if [[ "$users_guess" -lt 37 ]]; then
 	echo "that is way too low, are you trying to freeze?"
 
-elif [[ "$users_guess" -gt 37 ]]; then
-	echo " so you think we are that hot, boi are you wrong"
+	elif [[ "$users_guess" -gt 37 ]]; then
+		echo " so you think we are that hot, boi are you wrong"
 
+	else 
+		echo " you got it, someone is a smart boi"
 
-
-fi
-
+	fi
+else 
+	echo "plz just input an actual number, human being"
 
 # -lt = less than, -gt = greater than, 
 
+
+
+
+
+#-------------------------------------------------------------------------------------
+# like explained in brackets, if the number isnt a number you put the =~ ^[0-9]+$ ]] ,
+		# ^ is start of input,
 #( btw i somehow forgot the obvious things, life elif and the dollar sign, it should work now )
-# spacing in brackets is very important
+# spacing in brackets is very important  before and after you open the bracket
+
+#also colours make life so much easier in bash, everything should be colourful, not just sublime
