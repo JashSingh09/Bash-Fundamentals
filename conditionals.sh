22b5104bf6c96c7d7ff585eecc0a227bce815509
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-05 10:20:38 -0500

    my commits are kind of weird but i started conditionals properly

--- conditionals.sh
+++ conditionals.sh
@@ -4,5 +4,22 @@
 
 # conditionals are basically just logic tests, like boolean logic gates, like if a number is greater than 5 do something, thats it in simplest terms,
 
-#works with if commands
+#works with if commands usually,
 
+
+read -p "what temperature is the human body in celsius" users_guess
+
+if [["users_guess" == -lt 37]]; then
+	echo "that is way too low, are you trying to freeze?"
+
+else [["users_guess"  == -gt 37]]; then
+	
+
+
+
+fi
+
+
+# -lt = less than, -gt = greater than, 
+
+#
