ee9ef2b2afc1a42417bf46893677a3685a984483
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-06 10:42:17 -0500

    Mr Warren explained what %s does in printf

--- printecho.sh
+++ printecho.sh
@@ -6,3 +6,13 @@ printf "formatting is cool \nand we are cooked \n"
 
 # \n is for a new line
 
+# \t is for spacing( tab basically )
+
+#%s is for a blank space, like a variable, like you write the sentence then you call it by putting it at the end of the sentence
+
+
+# printf "Hello %s\n" "$name"
+
+#it says hello then puts in the variable that you put in, 
+
+
