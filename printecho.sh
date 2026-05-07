2f76e7ea4abd0a4f52588130836c3e995140c442
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-07 10:39:33 -0500

    i made the notes actually look like ntoes for printecho, but its only print right now

--- printecho.sh
+++ printecho.sh
@@ -7,6 +7,20 @@ printf "formatting is cool \nand we are cooked \n"
 # \n is for a new line
 
 # \t is for spacing( tab basically )
+# %d is for integess, like whole numbers
+# %f is for floating point, decimals numbers, with fractions,
+#	usually with 6 digits,
+#--------------------------------------------------------
+# OOOOH THERES MORE, 
+#	\\ is for actually getting a backslash
+#	\" is for double quotes
+# \a makes a sound which is cool
+# \b is backspace(i dont get it i will look at it later, hopefully)
+# \e is for colours, (also look into it after)
+
+# %10s aligns by 10 spaces on the right, -10 is the opposite direction
+ 
+
 
 #%s is for a blank space, like a variable, like you write the sentence then you call it by putting it at the end of the sentence
 
@@ -15,4 +29,4 @@ printf "formatting is cool \nand we are cooked \n"
 
 #it says hello then puts in the variable that you put in, 
 
-
+#
