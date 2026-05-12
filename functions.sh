3c15388e894db5ec466c8111fc9b0e20dd8e0ede
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-12 10:24:53 -0500

    fixed name of functions file and tried to understand what they are

---
+++ functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#functions are lines of code that can be called on when you need them for
+
+
+#start with the name of function
+#ie 
+greet_user() {
+	echo "whats wrong, $name_1"
+
+}
+
+read -p "can you put in your name for me"
+
+greet_user "
