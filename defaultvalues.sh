2ba44ded40b281555821a0e157ef56a7b2f8dfd0
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-13 10:41:04 -0500

    henry explained what default values mean and i kind of get it now

--- defaultvalues.sh
+++ defaultvalues.sh
@@ -4,3 +4,11 @@
 #--------------------------------------
 #from what i understand, its what happens when the the user does not provide input
 # for example if i ask for anything then, it will put default to fill up the input
+
+read -p "whats your name, to stay anonymous leave blank: " name 
+
+name =${name: -Anonymous}
+# you have to put a 
+#henry explained what default values meant
+# different syntax for different things
+#if i dont set anything it will put the name as anonymous
