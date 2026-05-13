e138ae4b7628d8148773dc46c407555f202a5efa
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-13 10:17:08 -0500

    its my first time hearing about default values, i dont know that much about it yet

---
+++ defaultvalues.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+#--------------------------------------
+#from what i understand, its what happens when the the user does not provide input
+# for example if i ask for anything then, it will put default to fill up the input
