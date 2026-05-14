bc6c458b83ca4a851b5d766cdfc9ee32e92b3bdf
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-14 10:27:18 -0500

    i messed up the info in he file, like functions has fully dissapeared becuase i was working on the wrong file(i needed to do default values)

--- defaultvalues.sh
+++ defaultvalues.sh
@@ -7,8 +7,27 @@
 
 read -p "whats your name, to stay anonymous leave blank: " name 
 
-name =${name: -Anonymous}
+name=${name:-Anonymous} #----------------------> will output anonymous when nothing is entered as that is the default value
 # you have to put a 
 #henry explained what default values meant
 # different syntax for different things
 #if i dont set anything it will put the name as anonymous
+#
+
+#-----------------------------------------------
+# name=${name-Anonymous}  -----------------------> 
+#-----------------------------------------------
+# name=${name=Anonymous}  -----------------------> 
+#-----------------------------------------------
+# name=${name:=Anonymous} ------------------------->  assigns default if empty and it changes the variable directly because it stores it
+#-----------------------------------------------
+#name=${name:+Anonymous} is if name has something then it will output king, ie user input is "king" it will still output anonymous
+#-----------------------------------------------
+
+#name=${name:?Name is required} ------------------> means if you dont put in anything ,t will say name is required
+
+# CONCLUSION
+# :-  use default if empty
+# :=  assign default if empty
+# :+  use alternate value if variable exists
+# :?  show error if empty
