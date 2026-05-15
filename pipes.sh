09544c5af6533a22cdc60dbc307525ae464c8e6d
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-15 10:03:35 -0500

    showed understanding of pipes, just hard to show example as it has a wide range

--- pipes.sh
+++ pipes.sh
@@ -2,5 +2,16 @@
 
 
 #pipes use things from the previous command and uses it
+#the output of one ommand as the input of the next
 
+# ie, 'ls -l | grep ".sh" ------------------------------> lists all the files and grep uses that list to only list the files that are .sh
 #
+
+# 
+read -p "enter words and i will tell you how many you inputted" words_inputted
+
+echo "$words_inputted" | wc -w
+
+# wc -w counts the words that you inputed
+# hard to show an exact exaples as they are very abstract, like you can use it for so many differnt things and this is just one example 
+
