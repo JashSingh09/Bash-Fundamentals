5254ca262e631943739d0f52aacd0244a0938517
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-15 10:46:03 -0500

    learnt a lot about redirects

--- redirects.sh
+++ redirects.sh
@@ -8,13 +8,15 @@
 #you can overwrite a file with use input
 
 # adds stdout
+read -p "what is your favorite food"
+
+echo "favorite food: $food" > food.txt
+
+#  puts the input from the favorite food into food.txt file which it creates
 
 
 
-
-
-
-
-
-
+# you can use cat after( NOTE TO WORK ON THIS LATER )
 #stdout is standard output
+
+#
