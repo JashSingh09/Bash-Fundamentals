97ef649a684fa20b4e45f32a4eb231636b3ec116
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-18 19:54:15 -0500

    halfway done redirects

--- redirects.sh
+++ redirects.sh
@@ -14,9 +14,11 @@ echo "favorite food: $food" > food.txt
 
 #  puts the input from the favorite food into food.txt file which it creates
 
+# echo "pizza >> food.txt would add it the end of a file and not remove anything
+#cat food.txt will output the favorite food and pizza in the terminal
+ 
 
-
-# you can use cat after( NOTE TO WORK ON THIS LATER )
+# you can use cat after to show the contents of the files, ( NOTE TO WORK ON THIS LATER )
 #stdout is standard output
 
 #
