4757afb2879ef6f99b2d2c979df573446c7d4912
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-18 21:27:59 -0500

    finished userimput( its peak now)

--- userinput.sh
+++ userinput.sh
@@ -20,5 +20,6 @@ echo "your name is $name1 and you like $food1"
 # read -p "prompt" stored_input
 
 
-
-
+#read -s  --------------------------------> silent input and doesnt show the user typing like when you type a sudo password in linux
+#read -n 1-------------------------------> is limited characters, in this case its only 1
+#read -t 5 ------------------------------> is timeout and waits 5 seconds for input then gives up
