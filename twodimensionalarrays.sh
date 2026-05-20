a1bc8955614b21024215c006cb3ccedc91c85641
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-20 10:37:39 -0500

    i think im done with 2darrays today

--- twodimensionalarrays.sh
+++ twodimensionalarrays.sh
@@ -18,6 +18,7 @@ for i in "${!students[@]}"; do
 	echo "${students[$i]} likes ${food[$i]}"
 done
 
+# so first i is kind of like a numbber counting loop, it goes through every number
 
 # chatgpt is trying to explain and it is saying that i can use i as a loop index variable
 # i hear there are 2 ways to simulate the 2darrays(shortfrom because its verylong), associative arrays and item 2d array simulation
