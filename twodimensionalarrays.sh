940e80bc74dddaf9804ab2b143b182e93f66c95a
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-20 09:56:36 -0500

    found the direction i want to take for 2dimensional arrays

--- twodimensionalarrays.sh
+++ twodimensionalarrays.sh
@@ -2,4 +2,10 @@
 
 
 #2 dimensional arrays are possible in a lot of other languages but not really in bash and they are like a spreadsheet, that have columns and rows,
-#
+#like we use the same index so it will show us both info, if that makes sense
+
+# Index | Student  | Food
+# ------|----------|------
+#   0   | Alice    | Pizza
+#   1   | Bob      | Ice Cream
+#   2   | Charlie  | Banana
