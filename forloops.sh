c1bb663b97e4536cabca3ac1b128dc428d4704ea
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-21 10:13:41 -0500

    basically done forloops

--- forloops.sh
+++ forloops.sh
@@ -11,9 +11,11 @@ fruits=("Apple" "Banana" "Strawberry")
 # for example I can make it loop echoing all the items in the array 
 for fooditem in "${fruits[@]}"
 do 
-	echo "$fooditem"
+	echo "$fooditem is a fruit"
 done
 # @ is every item in the array
 # so basically its like for every item in the array, echo (do) the output
-
-# 
+#NEED A DONE AT THE END OF IT
+# it goes thorough everything in the array
+#you can loop through number ranges and files
+#even loop throughh command results
