7505d4cb6f8587e392b6242ea2420a67d1db49eb
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-21 10:28:52 -0500

    worked on dowhileloops

--- dowhileloops.sh
+++ dowhileloops.sh
@@ -8,3 +8,15 @@ numbers=( 1 2 5 9 8 3 4 0 34 7 34 )
 
 # i want it it to go echo every number but when it equals 7 stop it
 
+index=0
+# so index is the order of the value, like 0 is 1 and 5 is 8
+
+while true
+do
+	#its going to repeatedly go through the loop
+	echo "${numbers[$index]}"
+
+	if [[ $numbers[$index]}
+
+
+#i keep on making the mistake of using the wrong brackets and miscounting what bracket should go where
