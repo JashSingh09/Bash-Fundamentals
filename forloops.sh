e9a8e2dbe6a554cfc6be300ec97d0a77e0789c19
Jashan Singh <180464234+JashSingh09@users.noreply.github.com>
2026-05-21 10:03:52 -0500

    started forloops

---
+++ forloops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#for loops repeat code 
+# they can repeat arrays lists and repeated tasks
+
+#you need done and 
+
+fruits=("Apple" "Banana" "Strawberry")
+
+# for example I can make it loop echoing all the items in the array 
